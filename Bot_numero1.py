04e4bd2ef9fd5d2b30b60b9a5467f95259b7c9a4
SofiViales <sofi.viales@gmail.com>
2025-03-26 18:11:52 -0600

    Create Bot_numero1.py

---
+++ Bot_numero1.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+bot.run("MTMzMzU4MDYxNTIwNjgzNDI3MQ.GJ0a8C.24h_u2h_P-046l4MNaWSbTf09SnhC_Z4kKswSg")
